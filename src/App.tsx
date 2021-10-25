7291111c6947f4c4773ce80f6eecf0a67280c474
kpose <ganihujude@gmail.com>
2021-10-25 10:11:26 +0100

    prepare and organize project directory

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text style={styles.headerText}> Accept Payment With Flutterwave!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerText: {
+    alignSelf: 'center',
+    fontWeight: 'bold',
+  },
+});
